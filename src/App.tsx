64f27e133bfd072a3b4791f63242eb0f9da197f6
thearturca <arti.1046@gmail.com>
2022-03-31 20:32:10 +0500

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import { MetaMaskInpageProvider } from '@metamask/providers';
+import { Maybe } from '@metamask/providers/dist/utils';
+import { useEffect, useState } from 'react';
+import Web3 from 'web3';
 import './App.css';
 
 function App() {
+
+  const [account, setAccount] = useState<string>(""); // state variable to set account.
+  
+  useEffect(() => {
+    async function load() {
+      const provider: MetaMaskInpageProvider = window.ethereum;
+
+      if (typeof provider === undefined) {
+        return;
+      }
+      const web3 = new Web3(Web3.givenProvider);
+      
+      const accounts = await web3.eth.requestAccounts();
+
+      if (accounts !== undefined && accounts !== null) {
+        if(accounts[0] !== undefined) {
+          setAccount(accounts[0]);
+        }
+      }
+    }
+    
+    load();
+   }, []);
+   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+       Your account is: {account}
+     </div>
     </div>
   );
 }
